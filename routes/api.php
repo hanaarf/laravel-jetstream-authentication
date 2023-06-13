e188083b498fbc3780cf66ba82b6fccdc2dc4d21
hanaarf <arifhana886@gmail.com>
2023-06-13 19:09:00 +0700

    tes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/register', [AuthController::class, 'Register']);
+Route::post('/auth/login', [AuthController::class, 'Login']);
+
+Route::get('/auth/siswa', [AuthController::class, 'jadwal']);
